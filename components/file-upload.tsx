59a91abc6939c1c3c04d16ddf69b1954393f0cb2
Shantilal Patliya <shantilalpatliya18@gmail.com>
2024-02-10 23:27:55 +0530

    uploadthing setup done

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -35,6 +35,7 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
+        console.log(res)
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
