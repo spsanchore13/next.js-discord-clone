dc25086b50af1ccca0e347822e34e0e24980fa5e
Shantilal Patliya <shantilalpatliya@outlook.com>
2024-03-06 23:57:19 +0530

    server sidebar inprogress

---
+++ components/server/server-header.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import { MemberRole } from "@prisma/client";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+
+interface ServerHeaderProps {
+  server: ServerWithMembersWithProfiles;
+  role: MemberRole;
+}
+
+const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none" asChild>
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+          {server.name}
+          <ChevronDown className="h-5 w-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
+
+export default ServerHeader;
