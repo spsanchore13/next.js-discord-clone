98e26c79e6154050f6a81e2763c9ead41e5eb82b
Shantilal Patliya <shantilalpatliya@outlook.com>
2024-03-09 23:16:57 +0530

    invite code join complete

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -11,12 +11,45 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Copy, RefreshCcw } from "lucide-react";
+import { Check, Copy, RefreshCcw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import axios from "axios";
 
 const InviteModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const origin = useOrigin();
+  const { server } = data;
 
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const isModalOpen = isOpen && type === "invite";
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`
+      );
+
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -32,23 +65,29 @@ const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-              value="invite-link"
+              value={inviteUrl}
             />
 
-            <Button size="icon">
-              <Copy className="w-4 h-4" />
-            </Button>
-
-            <Button
-              variant="link"
-              size="sm"
-              className="text-sm text-zinc-500 mt-4"
-            >
-              Generate a new link
-              <RefreshCcw className="w-4 h-4 ml-2" />
+            <Button disabled={isLoading} onClick={onCopy} size="icon">
+              {copied ? (
+                <Check className="w-4 h-4" />
+              ) : (
+                <Copy className="w-4 h-4" />
+              )}
             </Button>
           </div>
+          <Button
+            onClick={onNew}
+            disabled={isLoading}
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCcw className="w-4 h-4 ml-2" />
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
--- hooks/use-origin.ts
+++ hooks/use-origin.ts
@@ -1,3 +1,17 @@
-export const useOrigin = create()=>{
-    
+import { useEffect, useState } from "react"
+
+
+export const useOrigin = () => {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin : "";
+
+    if (!mounted) return ""
+
+    return origin;
+
 }
