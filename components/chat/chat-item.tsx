4279d50eeb19cbdd20ead0083e52d4cf88173ca1
Shantilal Patliya <shantilalpatliya@outlook.com>
2024-03-18 23:11:33 +0530

    msg display

---
+++ components/chat/chat-item.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Member, Profile } from "@prisma/client";
+
+interface ChatItemProps {
+  id: string;
+  content: string;
+  member: Member & {
+    profile: Profile;
+  };
+  timestamp: string;
+  fileUrl: string | null;
+  deleted: boolean;
+  currentMember: Member;
+  isUpdated: boolean;
+  socketUrl: string;
+  socketQuery: Record<string, string>;
+}
+
+const ChatItem = ({
+  id,
+  content,
+  member,
+  timestamp,
+  fileUrl,
+  deleted,
+  currentMember,
+  isUpdated,
+  socketUrl,
+  socketQuery,
+}: ChatItemProps) => {
+  return <div>ChatItem</div>;
+};
+
+export default ChatItem;
